5fba02b7cacd3a682526be8c6af2adcf0339e208
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-30 14:30:10 -0300

    6° commit - tdd tela Dayoffs: Existe tabela em Dayoffs? RED

--- src/tests/Dayoffs.test.js
+++ src/tests/Dayoffs.test.js
@@ -20,4 +20,8 @@ describe('Testes da tela Dayoffs', () => {
     expect(screen.getByTestId('mylink')).toBeInTheDocument();
   });
 
+  it('Existe tabela em Dayoffs?', () => {
+    expect(screen.getByTestId('mytable')).toBeInTheDocument();
+  });
+
 });
