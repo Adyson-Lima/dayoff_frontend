8bcb58ab44c1bec7c9abfaeb930c5e28a742de17
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-30 15:14:51 -0300

    24° commit - logica read implementada

--- src/pages/Dayoffs/index.js
+++ src/pages/Dayoffs/index.js
@@ -1,8 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-// import api from '../../services/api';
+import api from '../../services/api';
 
 export default function Dayoffs(){
+
+  const [my_dayoffs, setDayoffs] = useState([]);
+  const navigate = useNavigate();
+
+  // read, busca todos os registros da api
+  useEffect(() => {
+    api.get('api/v1/dayoffs', {})
+    .then(response => {setDayoffs(response.data)})
+  }, []);
 
   return(
 
@@ -18,17 +27,18 @@ export default function Dayoffs(){
         <table data-testid="mytable" className="table table-hover">
           <thead>
             <tr>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
-              <th scope="col">##</th>
+              <th scope="col">ID</th>
+              <th scope="col">Dia</th>
+              <th scope="col">Descrição</th>
+              <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row"></th>
-                <td></td>
-                <td></td>
+            {my_dayoffs.map(dayoff => (
+              <tr key={dayoff.id}>
+                <th scope="row">{dayoff.id}</th>
+                <td>{dayoff.day}</td>
+                <td>{dayoff.description}</td>
                 <td>
 
                   <button data-testid="mybtn1" type="button"
@@ -38,7 +48,9 @@ export default function Dayoffs(){
                   className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
 
                 </td>
-            </tr>
+              </tr>
+            ))}
+            
           </tbody>
         </table>
 
