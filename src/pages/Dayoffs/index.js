fadd6bc464ec11514e76f375fe7f209edc030d12
Adyson-Lima <adyson_lima@yahoo.com.br>
2024-01-30 15:23:59 -0300

    26° commit - logica delete implementada

--- src/pages/Dayoffs/index.js
+++ src/pages/Dayoffs/index.js
@@ -22,6 +22,16 @@ export default function Dayoffs(){
     }
   }
 
+  // delete, exclui um registro na api
+  async function deleteDayoff(id){
+    try {
+      await api.delete(`api/v1/dayoffs/${id}`,{});
+      setDayoffs(my_dayoffs.filter(dayoff => dayoff.id !== id));
+    } catch (error) {
+      alert('Erro ao excluir elemento');      
+    }
+  }
+
   return(
 
     <div data-testid="mycard" className="card border-primary" style={{marginTop: '20px'}} >
@@ -55,7 +65,8 @@ export default function Dayoffs(){
                   onClick={() => updateDayoff(dayoff.id)}>Editar</button>
 
                   <button data-testid="mybtn2" type="button"
-                  className="btn btn-outline-danger" style={{margin: '2px'}}>Excluir</button>
+                  className="btn btn-outline-danger" style={{margin: '2px'}}
+                  onClick={() => deleteDayoff(dayoff.id)}>Excluir</button>
 
                 </td>
               </tr>
